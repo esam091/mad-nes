c39f8346360c47c45bbaa64f543346cc35fa6b74
Samuel Edwin <esam091@gmail.com>
2021-03-20 20:29:21 +0700

    seems to work?

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,23 @@
-struct Machine {}
+struct Machine {
+    memory: [u8; 0xffff],
+}
 
-fn main() {
-    println!("Hello, world!");
+impl Machine {
+    pub fn load(file_path: &String) -> Result<Machine, std::io::Error> {
+        let bytes = std::fs::read(file_path)?;
+
+        let mut memory: [u8; 0xffff] = [0; 0xffff];
+
+        for i in 0..bytes.len() {
+            memory[0x8000 + i] = bytes[i];
+        }
+
+        return Ok(Machine { memory: memory });
+    }
+}
+
+fn main() -> Result<(), String> {
+    let machine = Machine::load(&String::from("hello.nes")).unwrap();
+
+    Ok(())
 }
