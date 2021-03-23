fcec78f80bc67658313817885afc02e534fdb5b3
Samuel Edwin <esam091@gmail.com>
2021-03-23 10:24:56 +0700

    things are working for now

--- src/main.rs
+++ src/main.rs
@@ -18,13 +18,19 @@ struct Machine {
 
 impl Machine {
     pub fn load(file_path: &String) -> Result<Machine, std::io::Error> {
+        // since we're using a legit hello world ROM as a prototype, we don't need to configure stuff from headers
         let bytes: Vec<u8> = std::fs::read(file_path)?.into_iter().skip(16).collect();
 
         let mut memory = [0 as u8; 0x10000];
 
-        for i in 0..bytes.len() {
+        for i in 0..bytes.len() - 16 {
             memory[0x8000 + i] = bytes[i];
         }
+
+        // Copy the reset vector data
+        // The data was located at 400c but we subtract by 0x10 since we skipped 16 bytes in the header
+        memory[0xfffc] = bytes[0x3ffc];
+        memory[0xfffd] = bytes[0x3ffd];
 
         return Ok(Machine { memory: memory });
     }
@@ -103,4 +109,17 @@ fn main() -> Result<(), String> {
         .map_err(|_| "Failed drawing terminal")?;
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn endian_conversion() {
+        assert_eq!(u16::from_le_bytes([0xbb, 0xaa]), 0xaabb);
+        assert_eq!(u16::from_le(0xaabb), 0xaabb);
+        assert_eq!(u16::from_be_bytes([0xaa, 0xbb]), 0xaabb);
+        assert_eq!(u16::from_be(0xbbaa), 0xaabb);
+        assert_eq!(0b1111, 15);
+    }
 }
