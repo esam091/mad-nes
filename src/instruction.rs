58ca707499163d0f7498734434c4e6abcf57a614
Samuel Edwin <esam091@gmail.com>
2021-03-29 00:41:58 +0700

    Knowledge about cycles is put into the CPU
    Because some cycle counting depends of the computation result

--- src/instruction.rs
+++ src/instruction.rs
@@ -11,21 +11,3 @@ pub enum Instruction {
     CpxImmediate(u8),
     Bne(u8),
 }
-
-impl Instruction {
-    pub fn cycles(&self) -> u32 {
-        match self {
-            Instruction::LdaImmediate(_)
-            | Instruction::LdxImmediate(_)
-            | Instruction::CmpImmediate(_)
-            | Instruction::CpxImmediate(_)
-            | Instruction::Beq(_)
-            | Instruction::Bne(_)
-            | Instruction::Inx => 2,
-
-            Instruction::StaAbsolute(_) | Instruction::LdaXAbsolute(_) => 4,
-
-            Instruction::JmpAbsolute(_) => 5,
-        }
-    }
-}
